eeb34de60deac0da57137c35b9599be74b892ec9
Muhammad <mhmdshiddiq17@gmail.com>
2024-05-15 01:02:37 +0700

    commit again am

--- src/pages/user/Artikel.jsx
+++ src/pages/user/Artikel.jsx
@@ -2,27 +2,32 @@ import React from 'react';
 import Artikel1 from './../../assets/artikel/artikel1.png';
 import Artikel2 from './../../assets/artikel/artikel2.png';
 import Artikel3 from './../../assets/artikel/artikel3.jpg';
+import Artikel4 from './../../assets/artikel/artikel4.png';
+import Artikel6 from './../../assets/artikel/artikel6.png';
+import Artikel7 from './../../assets/artikel/artikel7.png';
+import Artikel8 from './../../assets/artikel/artikel8.png';
+import Artikel9 from './../../assets/artikel/artikel9.png';
 import { IoIosSearch } from "react-icons/io";
 import { Link } from 'react-router-dom';
 
 
 const DataArtikel = [
 	{
-		title: 'Mentan Ingin Bangun Klaster Pertanian Modern', timestamps: '07 Mei 2024 15:52 WIB', description: 'Kementerian Pertanian (Kementan) berencana untuk membangun klaster pertanian modern. Klaster pertanian modern tersebut mengacu sistem pertanian di negara-negara maju. Hal tersebut diungkapkan Mentan Andi Amran Sulaiman saat mendatangi para petani Bojongemas, Kecamatan Solokanjeruk, Kabupaten Bandung, Selasa (7/5/2024). Menurut Amran, demi merealisasikan pertanian modern, pemerintah daerah perlu menyediakan lahan.'
+		image: Artikel1,title: 'Mentan Ingin Bangun Klaster Pertanian Modern', timestamps: '07 Mei 2024 15:52 WIB', description: 'Kementerian Pertanian (Kementan) berencana untuk membangun klaster pertanian modern. Klaster pertanian modern tersebut mengacu sistem pertanian di negara-negara maju. Hal tersebut diungkapkan Mentan Andi Amran Sulaiman saat mendatangi para petani Bojongemas, Kecamatan Solokanjeruk, Kabupaten Bandung, Selasa (7/5/2024). Menurut Amran, demi merealisasikan pertanian modern, pemerintah daerah perlu menyediakan lahan.'
 	},
 	{
-		title: 'PJ Bupati Palas Edy Junaedi Salurkan Bantuan Alsintan ke Petani', timestamps: '07 Mei 2024 14:40 WIB', description: 'Pj Bupati Padang Lawas (Palas) Edy Junaedi menyampaikan sejumlah langkah yang akan dilakukan untuk meningkatkan produksi pangan masyarakat. Satu di antaranya, memaksimalkan bantuan dari kementerian berupa alat dan mesin pertanian (Alsintan)'
+		image: Artikel2, title: 'PJ Bupati Palas Edy Junaedi Salurkan Bantuan Alsintan ke Petani', timestamps: '07 Mei 2024 14:40 WIB', description: 'Pj Bupati Padang Lawas (Palas) Edy Junaedi menyampaikan sejumlah langkah yang akan dilakukan untuk meningkatkan produksi pangan masyarakat. Satu di antaranya, memaksimalkan bantuan dari kementerian berupa alat dan mesin pertanian (Alsintan)'
 	},
 
 ]
 
 const SampingArtikel = [
-		{judulArtikel: `Optimalisasi Produksi Tanaman: Panduan Praktis untuk Mengatur pH Tanah`, timestamps: '2 Mei 2024'},
-		{judulArtikel: `Pemerintah Aceh Gelora Kembali Pasar Tani di 2024`, timestamps: '25 April 2024'},
-		{judulArtikel: `Apakah Makanan Organik Benar-benar Sehat?`, timestamps: '25 April 2024'},
-		{judulArtikel: `Stok Pupuk Bersubsidi Melimpah, Petani Diminta Menebus Kuota ynag Dimiliki`, timestamps: '25 April 2024'},
-		{judulArtikel: `Violet Sun Agro+, Solusi Pertanian Lahan Kering Bertenaga Surya`, timestamps: '25 April 2024'},
-		{judulArtikel: `6 Pengaruh Negatif IPTEK di Bidang Pertanian`, timestamps: '25 April 2024'},
+		{judulArtikel: `Optimalisasi Produksi Tanaman: Panduan Praktis untuk Mengatur pH Tanah`, timestamps: '2 Mei 2024', image: Artikel3},
+		{judulArtikel: `Pemerintah Aceh Gelora Kembali Pasar Tani di 2024`, timestamps: '25 April 2024', image: Artikel4},
+		{judulArtikel: `Apakah Makanan Organik Benar-benar Sehat?`, timestamps: '25 April 2024', image: Artikel6},
+		{judulArtikel: `Stok Pupuk Bersubsidi Melimpah, Petani Diminta Menebus Kuota ynag Dimiliki`, timestamps: '25 April 2024',image: Artikel7},
+		{judulArtikel: `Violet Sun Agro+, Solusi Pertanian Lahan Kering Bertenaga Surya`, timestamps: '25 April 2024', image: Artikel8},
+		{judulArtikel: `6 Pengaruh Negatif IPTEK di Bidang Pertanian`, timestamps: '25 April 2024', image: Artikel9},
 ]
 
 // const SampingArtikel = [
@@ -36,29 +41,37 @@ const SampingArtikel = [
 
 
 
-const Line = () => {
-  const lineStyle = {
-    height: '850px',
-    border: '1px solid #807D7E',
-    width: '2px',
-  };
-
+const Line = (props) => {
+  // const lineStyle = {
+  //   height: '850px',
+  //   border: '1px solid #807D7E',
+  //   width: '2px',
+  // };
   return (
-    <div style={lineStyle}></div>
+    <div style={props.style}></div>
   );
 };
 
 const MainArtikel = () =>{
+	const horizStyle = {
+		height: '2px',
+		width: '470px',
+		border: '1px solid #807D7E',
+		marginTop: '50px'
+	}
 	return(
 		<div className='grid grid-rows-2 gap-y-16'>
 				{DataArtikel.map((news, index) => (
-				<div className='flex flex-col mx-32 gap-y-2' key={index}>
-					<img src={Artikel1} alt="" className='w-[470px]'/>
-					<p className='text-[12px] text-[#807D7E]'>{news.timestamps}</p>
-					<Link to={'/'} className='text-[32px] font-semibold w-[430px]'>{news.title}</Link>
-					<p className='w-[430px] text-sm'>{news.description}</p>
-				</div>
+					<div className='flex flex-col mx-32 gap-y-2' key={index}>
+						<img src={news.image} alt="" className='w-[470px]'/>
+						<p className='text-[12px] text-[#807D7E]'>{news.timestamps}</p>
+						<Link to={'/'} className='text-[32px] font-semibold w-[430px]'>{news.title}</Link>
+						<p className='w-[430px] text-sm'>{news.description}</p>
+						<Line style={horizStyle}/>
+					</div>
 				))}
+				<div>
+				</div>
 		</div>
 	)
 }
@@ -68,9 +81,9 @@ const ChildArtikel = () =>{
 		<div>
 		{SampingArtikel.map((tes, index) => (
 			<div className='flex flex-row mx-32 gap-y-2 mb-7' key={index}>
-					<img src={Artikel3} alt="" className='w-24 rounded-2xl'/>
+					<img src={tes.image} alt="" className='w-24 rounded-2xl'/>
 					<div className='ms-3 w-40 flex flex-col justify-between'>
-						<p className="text-[12px] font-bold">{tes.judulArtikel}</p>
+						<Link className="text-[12px] font-bold" to={'/'}>{tes.judulArtikel}</Link>
 						<p className='text-xs font-thin text-[#807D7E]'>{tes.timestamps}</p>
 					</div>
 			</div>
@@ -80,7 +93,13 @@ const ChildArtikel = () =>{
 }
 
 
-const Artikel = () => {
+const Artikel = (props) => {
+	const lineStyle = {
+    height: '850px',
+    border: '1px solid #807D7E',
+    width: '2px',
+  };
+
   return (
     <div style={{backgroundColor: '#ffffff', backgroundImage: 'linear-gradient(#e6e6e6,#ffffff)'}}>
     	<div className='pt-48 text-center'>
@@ -94,7 +113,7 @@ const Artikel = () => {
 			<div className='flex pt-48 pb-16'>
 				<MainArtikel />
 				<div>
-					<Line />
+					<Line style={lineStyle}/>
 				</div>
 				<ChildArtikel />
 					{/* <div className='flex flex-row flex-wrap mx-32 gap-y-2'>
